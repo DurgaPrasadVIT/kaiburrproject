f7b37629c64d946d081874082f376ec25efc4db2
Durga Prasad <gdurgaprasad950@gmail.com>
2022-04-10 17:46:42 +0530

    Sample

---
+++ src/main/java/com/kaiburr/api/kaiburrproject/KaiburrprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kaiburr.api.kaiburrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiburrprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiburrprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaiburr/api/kaiburrproject/KaiburrprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiburr.api.kaiburrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiburrprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
